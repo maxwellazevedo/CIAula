f5a6539e76262cdc3dff0e7f3e71a841dd9faae4
Maxwell Azevedo da Silva <maxwell.azevedo@DCX.UFPB.BR>
2016-05-24 10:54:41 -0300

    Adicionando Travis no projeto

---
+++ src/main/java/pratica_IA/pratice/App.java
@@ -0,0 +1,13 @@
+package pratica_IA.pratice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
